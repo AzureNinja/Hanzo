b8864dc13bc92ad8fdc2e0518995b8156ce65c29
Yoichi Kawasaki <yoichika@microsoft.com>
2016-07-26 13:16:34 +0900

    Initial commit

---
+++ HanzoProxyAPI/Models/VirtualMachineObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HanzoProxyAPI.Models
+{
+    public class VirtualMachineObject
+    {
+        public string name { get; set; }
+        public string resourcegroup { get; set; }
+        public string status { get; set; }
+    }
+}
